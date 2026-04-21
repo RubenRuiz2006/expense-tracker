4154d77f285f5f06011ae762e0c780d87f872b71
Rubén Ruiz Mayorga <rubenruizmay18@gmail.com>
2026-04-21 09:27:06 +0200

    disposición de botón

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,18 @@
-import { Link } from "react-router-dom"
-export default function Navbar(){
-      return (
+import { useLocation, Link } from 'react-router-dom'
+
+export default function Navbar() {
+  const location = useLocation()
+  const enResumen = location.pathname === '/resumen'
+
+  return (
     <nav className="bg-blue-600 text-white px-6 py-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">Expense Tracker</h1>
       <div className="flex gap-4">
-        <Link to="/" className="hover:underline">
-          Gastos
-        </Link>
-        <Link to="/resumen" className="hover:underline">
-          Resumen
-        </Link>
+        {enResumen ? (
+          <Link to="/" className="hover:underline">Ver gastos</Link>
+        ) : (
+          <Link to="/resumen" className="hover:underline">Ver resumen</Link>
+        )}
       </div>
     </nav>
   )
